2832f524f1c85b04bbae5d40eff2b5cb099578ba
talialy <allysonnomuraga@gmail.com>
2022-04-22 20:27:31 -0500

    solving & pushing

--- rolling.sh
+++ rolling.sh
@@ -1,3 +1,10 @@
+read -n1 -p "You want to install everything? It will take a long time" doit 
+case $doit in  
+  y|Y) echo yes ;; 
+  n|N) exit ;; 
+  *) echo I can\'t recognize that, so I will cancel it ;; 
+esac
+
 sudo pacman -Suy
 
 # o
@@ -5,12 +12,9 @@ mkdir ~/Downloads
 mkdir ~/Images
 mkdir ~/Videos
 mkdir ~/Projects
+cp -R ./Support/ ~/Support
 
-sleep 1
-echo "the main folders were made"
-sleep 2
-
-# again o?
+sleep 1 && echo "the main folders were made"
 
 ## yay!  bin
 
@@ -26,18 +30,21 @@ sleep 1
 echo "You have installed fish!"
 sleep 1
 
-sudo pacman -Sy --needed noto-fonts-emoji ttf-droid htop ranger feh
+sudo pacman -Suy --needed noto-fonts-emoji ttf-droid htop ranger feh
 
 ## it's time for the window manager!
 
-sleep 1 && echo "some apps right? :>" && sleep 1
+sleep 1
+echo """some apps right? :>
+"""
+sleep 1
 
-sudo pacman -Sy --needed rofi picom alacritty nemo dunst xorg-xinit ffmpegthumbnailer unzip zip unrar
+sudo pacman -Suy --needed rofi picom alacritty nemo dunst xorg-xinit ffmpegthumbnailer unzip zip unrar
 yay -S microsoft-edge-stable-bin
 
 sleep 1 && echo "just wanted to let you know I'm going to install the i3-gaps" sleep 1
 
-sudo pacman -Sy --needed i3-gaps i3blocks pfetch-btw
+sudo pacman -Suy --needed i3-gaps i3blocks
 
 ## now for the things about the install, I mean, the normal, I mean-
 
